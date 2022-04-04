a44f65d4c350bf339c3f7805d7d5eb4ed5819978
Philipad1234 <92040043+Philipad1234@users.noreply.github.com>
2022-04-04 12:02:55 +0000

    My first React Commit

---
+++ react-props-practice/src/contacts.js
@@ -0,0 +1,26 @@
+const contacts = [
+  {
+    name: "Tom Hardy",
+    imgURL:
+      "https://cdn.britannica.com/38/188638-050-08A5704E/Tom-Hardy-British-2014.jpg",
+    phone: "+123 456 789",
+    email: "Tom@Hardy.com"
+  },
+  {
+    name: "Damson Idris",
+    imgURL:
+      "https://m.media-amazon.com/images/M/MV5BMGJjNDczYzktNjIzOS00OWE3LWE2YjItZDM0YmE5ZmFkNTgzXkEyXkFqcGdeQXVyMjkyMzI3MjU@._V1_.jpg",
+    phone: "+987 654 321",
+    email: "IdrisDam@nowhere.com"
+  },
+  {
+    name: "Lori Harvey",
+    imgURL:
+      "https://www.refinery29.com/images/10462140.jpg",
+    phone: "+918 372 574",
+    email: "Lorharv@gmail.com"
+  }
+];
+
+
+export default contacts;
